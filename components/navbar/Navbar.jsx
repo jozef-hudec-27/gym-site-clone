ec2a428e5d6904efe56b9dcc7092766612492bdb
Jozef Hudec <jojohudec27@gmail.com>
2023-07-03 20:38:50 +0200

    Make navbar responsive

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
   ]
 
   return (
-    <nav className="flex justify-center items-center gap-5 py-4">
+    <nav className="flex flex-col xl:flex-row justify-center items-center gap-5 py-4 px-2">
       <Link href="/">
         <Image src="/assets/images/logo.webp" alt="Gym logo" height={0} width={250} />
       </Link>
@@ -28,49 +28,45 @@ function Navbar() {
         <BsInstagram className="text-xl" aria-label="Instagram" />
       </div>
 
-      <div className="flex gap-px justify-end items-center">
+      <div className="flex gap-px justify-center items-center flex-wrap">
         {/* LINKS */}
-        <div className="flex items-stretch gap-px">
-          <div className="bg-gray-200 self-stretch">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-          {navLinks.map((navLink, i) => {
-            return <NavLink key={i} navLink={navLink} />
-          })}
+        <div className="bg-gray-200 hidden xl:block">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </div>
+        {navLinks.map((navLink, i) => {
+          return <NavLink key={i} navLink={navLink} />
+        })}
 
         {/* DROPDOWNS */}
-        <div className="flex gap-px">
-          <div className="nav-dropdown-btn" tabIndex={0}>
-            Member portal
-            <div className="dropdown">
-              <Link className="drop-link" href="/">
-                Login
-              </Link>
-              <Link className="drop-link" href="/">
-                Membership cancellation
-              </Link>
-              <Link className="drop-link" href="/">
-                Membership freeze
-              </Link>
-            </div>
+        <div className="nav-dropdown-btn" tabIndex={0}>
+          Member portal
+          <div className="dropdown">
+            <Link className="drop-link" href="/">
+              Login
+            </Link>
+            <Link className="drop-link" href="/">
+              Membership cancellation
+            </Link>
+            <Link className="drop-link" href="/">
+              Membership freeze
+            </Link>
           </div>
+        </div>
 
-          <div className="nav-dropdown-btn" tabIndex={0}>
-            More
-            <div className="dropdown">
-              <Link className="drop-link" href="/">
-                Contact
-              </Link>
-              <Link className="drop-link" href="/">
-                Gift card
-              </Link>
-            </div>
+        <div className="nav-dropdown-btn" tabIndex={0}>
+          More
+          <div className="dropdown">
+            <Link className="drop-link" href="/">
+              Contact
+            </Link>
+            <Link className="drop-link" href="/">
+              Gift card
+            </Link>
           </div>
         </div>
 
         {/* SHOPPING CART */}
-        <Link href="/" className="flex justify-center items-center text-5xl ml-4 text-blue-500">
+        <Link href="/" className="hidden xl:flex justify-center items-center text-5xl ml-4 text-blue-500">
           <AiFillShopping aria-label="Shopping bag" />
         </Link>
       </div>
