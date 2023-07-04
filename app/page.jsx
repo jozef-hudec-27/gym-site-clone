8b7741ce8aa14efe4cc615d14aaf87739b3f6230
Jozef Hudec <jojohudec27@gmail.com>
2023-07-04 09:31:22 +0200

    Add gym description section to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 function HomePage() {
   return (
     <div>
@@ -11,6 +13,22 @@ function HomePage() {
         <button className="uppercase py-2 px-4 bg-orange-400 font-semibold hover:text-white hover:bg-blue-500 duration-[400ms] border-solid border-black border drop-shadow-2xl">
           Guess pass
         </button>
+      </section>
+
+      <section className="flex justify-between gap-3 w-10/12 mx-auto mt-10">
+        <div className="bg-black flex-center p-5 w-1/2">
+          <div className="flex flex-col gap-12 items-center w-2/3">
+            <h2 className="text-white text-5xl font-semibold">THE GYM L.A.</h2>
+            <p className="text-white text-justify">
+              Real people working really hard, to get real results. At THE GYM L.A your fitness is our #1 priority. From
+              the moment you walk in the doors, you know what to expect.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-1/2">
+          <Image src="/assets/images/gym.webp" width={863} height={100} />
+        </div>
       </section>
     </div>
   )
